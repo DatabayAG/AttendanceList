4755cad31167b9fb6e4f9a1349a31909104da408
Marvin Beym <mBeym@databay.de>
2024-02-14 11:44:03 +0100

    Update plugin.php & composer.json

--- plugin.php
+++ plugin.php
@@ -1,9 +1,8 @@
 <?php
-
 $id = 'xali';
-$version = "2.0.5";
-$ilias_min_version = '9.0';
-$ilias_max_version = '9.999';
-$responsible = 'fluxlabs';
-$responsible_mail = 'support@fluxlabs.ch';
+$version = '2.0.0';
+$ilias_min_version = '8.0';
+$ilias_max_version = '8.999';
+$responsible = "mbeym | mjansen";
+$responsible_mail = "mbeym@databay.de mjansen@databay.de";
 $learning_progress = true;
